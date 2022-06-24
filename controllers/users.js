2292c7565fe156e274a27f3eecf44ab4cced00f6
Diana Nurgaleeva <NurgaleevaDK@yandex.ru>
2022-06-24 08:06:39 +0300

    User errors

--- controllers/users.js
+++ controllers/users.js
@@ -36,7 +36,7 @@ module.exports.updateUser = (req, res) => {
   User.findByIdAndUpdate(
     req.user._id,
     { name: req.body.name, about: req.body.about },
-    { new: true },
+    { new: true, runValidators: true },
   )
     .then((user) => res.status(200).send({ data: user }))
     .catch((err) => {
