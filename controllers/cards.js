1d0e8f495b68faefa9de0c8a256137bf6dea7517
Diana Nurgaleeva <NurgaleevaDK@yandex.ru>
2022-06-25 07:21:26 +0300

    Card errors

--- controllers/cards.js
+++ controllers/cards.js
@@ -8,7 +8,8 @@ module.exports.getCards = (req, res) => {
 module.exports.createCard = (req, res) => {
   const { name, link } = req.body;
   const owner = req.user._id;
-  Card.create({ name, link, owner })
+  const likes = [];
+  Card.create({ name, link, owner, likes })
     .then((card) => res.status(200).send({ data: card }))
     .catch((err) => {
       if (err.name === 'ValidationError') {
