525120f58845a790ee348ba6d9d7fda1a718c8cf
Diana Nurgaleeva <NurgaleevaDK@yandex.ru>
2022-07-05 13:14:07 +0300

    PR 14 card error create with Joi

--- routes/cards.js
+++ routes/cards.js
@@ -15,7 +15,7 @@ router.post(
   celebrate({
     body: Joi.object().keys({
       name: Joi.string().required().min(2).max(30),
-      link: Joi.string().required(),
+      link: Joi.string().required().pattern(/(https?:\/\/)(w{3}\.)?(((\d{1,3}\.){3}\d{1,3})|((\w-?)+\.(ru|com)))(:\d{2,5})?((\/.+)+)?\/?#?/),
     }),
   }),
   createCard,
