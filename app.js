b53bdb5386736a4c9572e844916249af05e5074b
Diana Nurgaleeva <NurgaleevaDK@yandex.ru>
2022-07-05 15:00:25 +0300

    PR 14 user signUp  with Joi

--- app.js
+++ app.js
@@ -15,14 +15,6 @@ const { auth } = require('./middlewares/auth');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// app.use((req, res, next) => {
-//   req.user = {
-//     _id: '62b2886c4145914626b0fe40',
-//   };
-
-//   next();
-// });
-
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
 app.use('/users', auth, require('./routes/users'));
